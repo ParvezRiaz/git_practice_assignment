bbcef59341a04efb749e7a99d526f5f16657dcc6
cakestuna <rithik8595@gmail.com>
2024-02-29 14:52:30 +0530

    i fix the code

--- prime.js
+++ prime.js
@@ -3,14 +3,16 @@ function identifyPrime(num) {
 
   if (num <= 1) {
     console.log("No");
-    return;
+   
   }
 
   for (let i = 2; i * i <= num; i++) {
     if (num % i === 0) {
       console.log("No");
-      return;
+    
     }
   }
   console.log("Yes");
 }
+
+identifyPrime(62)
